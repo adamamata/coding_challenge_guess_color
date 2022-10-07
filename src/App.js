c51f0be9a93906ecdd1fc88dd6d391ee4092b8da
Adam Amatavivadhana <adam.amata@yahoo.com>
2022-10-07 13:20:10 +0200

    generate options, buttons, and color

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  //States
+  const [color, setColor] = useState("");
+  const [options, setOptions] = useState([]);
+
+  //Function to generate a new color 
+  function getRandomColor(){
+    const characters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
+    const newColorArr = [];
+    for (let i = 0; i < 6; i++){
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      newColorArr.push(characters[randomIndex]); 
+    }
+    return `#${newColorArr.join("")}`;
+  }
+
+  //Onload 
+  useEffect(() => {
+    const correctOption = getRandomColor();
+    setColor(correctOption);
+    setOptions([correctOption, getRandomColor(), getRandomColor()]);
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="color-box" style={{backgroundColor: color}}></div>
+      <div className='options'>
+        {options.map(option => {
+          return (
+            <button>{option}</button>
+          )
+        })}
+      </div>
     </div>
   );
 }
